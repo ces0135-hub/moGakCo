041ec341e8915e2e8de1e85708a513a34cec4291
erica0321 <hyunahrho29@gmail.com>
2022-08-21 19:34:48 +0900

    페이지

---
+++ js/slide.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+    $('#slide>ul').delay(2500);
+    $('#slide>ul').animate({marginLeft: "-1200px"})
+    $('#slide>ul').delay(2500);
+    $('#slide>ul').animate({marginLeft: "-2400px"})
+    $('#slide>ul').delay(2500);
+    $('#slide>ul').animate({marginLeft: "0px"})
+});
